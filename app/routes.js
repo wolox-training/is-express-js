fa8959bef90aa5151832efd12938abb1e91c00de
igsosa92 <ignacio.sosa@wolox.com.ar>
2018-10-17 13:16:46 -0300

    add useless changes

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,3 @@
-<<<<<<< 0245e35d56d825ec245c3c28f1df33f6c645a844
-const userController = require('./controllers/users'),
-  userMiddleware = require('./middlewares/users');
-
-exports.init = app => {
-  app.get('/users/:page', [userMiddleware.tokenValidation], userController.printSomeUser);
-  app.post('/users', [userMiddleware.signUpValidation], userController.create);
-  app.post('/users/sessions', [userMiddleware.signInValidation], userController.login);
-  app.post('/admin/users', [], );
-=======
 const uCtrl = require('./controllers/users'),
   uMW = require('./middlewares/users');
 
@@ -15,18 +5,10 @@ exports.init = app => {
   app.get('/users/:page', [uMW.tokenValidation], uCtrl.printSomeUser);
   app.post('/users', [uMW.signUpValidation], uCtrl.create);
   app.post('/users/sessions', [uMW.signInValidation], uCtrl.login);
-<<<<<<< 6f7473164c0c8793d2d8ed0d09e5228f552630cc
-  app.post('/admin/users', [uMW.tokenValidation, uMW.adminValidation, uMW.signUpValidation, uMW.updateValidation], uCtrl.createOrUpdateAdminUser );
->>>>>>> Add user-admin features
-=======
   app.post(
     '/admin/users',
     [uMW.tokenValidation, uMW.adminValidation, uMW.signUpValidation, uMW.updateValidation],
     uCtrl.createOrUpdateAdminUser
   );
-<<<<<<< 0faf974191f66f32fbaac485d8d2764036f0c00d
->>>>>>> add all features of admin user
-=======
   app.post('/admin/users/test', [], uCtrl.createAdminForTest);
->>>>>>> Add all test
 };
